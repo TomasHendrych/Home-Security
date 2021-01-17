d72e3702610ed07f7fa1333f76ce9e8652ea6319
TomasHendrych <36028668+TomasHendrych@users.noreply.github.com>
2021-01-17 13:54:15 +0100

    Add files via upload

--- Home-Security/src/main.cpp
+++ Home-Security/src/main.cpp
@@ -4,23 +4,10 @@
 #include <Password.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
-//#include <WiFi.h>
-
-/*
-  const char* ssid = "";
-  const char* password = "";
-
-  const char* http_username = "admin";
-  const char* http_password = "admin";
-
-  const char* PARAM_INPUT_1 = "state";
-*/
 
 int redLed = 32; //cervena 
 int greenLed = 15; //zelena
 int pir = 33; //pir sensor
-//int processor;
-//int ledPin;
 
 Password password = Password( "2345" );
 
@@ -160,44 +147,6 @@ void setup(){
   pinMode(redLed, OUTPUT); //zelena led
   pinMode(greenLed, OUTPUT); //cervena led
   pinMode(pir, INPUT); //pir
-
-  /*
-  // Initialize SPIFFS
-  if(!SPIFFS.begin(true)){
-    Serial.println("An Error has occurred while mounting SPIFFS");
-    return;
-  }
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
-  }
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to load style.css file
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(SPIFFS, "/style.css", "text/css");
-  });
-  // Route to set GPIO to HIGH
-  server.on("/on", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, HIGH);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  
-  // Route to set GPIO to LOW
-  server.on("/off", HTTP_GET, [](AsyncWebServerRequest *request){
-    digitalWrite(ledPin, LOW);    
-    request->send(SPIFFS, "/index.html", String(), false, processor);
-  });
-  server.begin();
-  */
- 
 }
 
 void loop(){
